46137eece8e1023e8604c97263548eea7702b4bf
RestartFU <zerkoming@gmail.com>
2023-08-25 21:38:21 -0400

    hooks update

--- webhook.go
+++
@@ -1,7 +0,0 @@
-package carrot
-
-import "github.com/moyai-network/carrot/webhook"
-
-var BanHook = webhook.NewHook("1144805219159523388", "gSG9jfTV7JKgArWUdF0nkdzQMcSw_dBuAh7q5NcONFjqnW1ejQp5")
-var MuteHook = webhook.NewHook("1144805514425933875", "9Kz6ueRmoOpqQGNyRc6XovycRtX5aiFWY9EMr6tuFIxh9VKX2Cj0yCq3278244sz0gGa")
-var OomphHook = webhook.NewHook("1144805598161023029", "dzIY1um5AaQnZvrruPMYmf35eHi9KiiEOE5Rq1My")
---
+++ webhook/punishment.go
@@ -0,0 +1,35 @@
+package webhook
+
+import "fmt"
+
+var banHook = NewHook("1144805219159523388", "gSG9jfTV7JKgArWUdF0nkdzQMcSw_dBuAh7q5NcONFjqnW1ejQp5")
+var muteHook = NewHook("1144805514425933875", "9Kz6ueRmoOpqQGNyRc6XovycRtX5aiFWY9EMr6tuFIxh9VKX2Cj0yCq3278244sz0gGa")
+var oomphHook = NewHook("1144805598161023029", "dzIY1um5AaQnZvrruPMYmf35eHi9KiiEOE5Rq1My")
+
+type Punishment struct {
+	p string
+	h Hook
+}
+
+func BanPunishment() Punishment {
+	return Punishment{"Ban", banHook}
+}
+
+func MutePunishment() Punishment {
+	return Punishment{"Mute", muteHook}
+}
+
+func OomphPunishment() Punishment {
+	return Punishment{"Oomph", oomphHook}
+}
+
+func SendPunishment(staff, victim, reason string, punishment Punishment) {
+	var payload Payload
+	payload.Embeds = []Embed{
+		{
+			Title:       punishment.p,
+			Description: fmt.Sprintf("**Staff:** %s\n**Victim:** %s\n**Reason:** %s", staff, victim, reason),
+		},
+	}
+	_ = punishment.h.SendMessage(payload)
+}
